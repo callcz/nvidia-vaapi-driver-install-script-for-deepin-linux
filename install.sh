dc6d27e7853f3343b5f480d3d19e690b76d09a79
callcz <66212678+callcz@users.noreply.github.com>
2022-06-02 23:57:24 +0800

    Update install.sh
    Optimize code

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-#upgrade installed packages
+##upgrade installed packages
 #i386 packages
-#i386_insed=$(apt list --installed $(ls depends/*i386.deb |cut -d/ -f2|cut -d_ -f1)|grep i386|cut -d/ -f1)
-#i386_ins=$(echo $i386_insed |sed 's/ /*i386.deb /g'|sed 's/$/&*i386.deb/g')
-i386_ins=$(apt list --installed $(ls depends/*i386.deb |cut -d/ -f2|cut -d_ -f1)|grep i386|cut -d/ -f1|sed 's/$/&*i386.deb/g')
+i386_ins=$(apt list --installed $(ls depends/*amd64.deb |cut -d/ -f2|cut -d_ -f1)|grep amd64|cut -d/ -f1 |sed 's/$/&*amd64.deb /g')
+#amd64 packages
+amd64_ins=$(apt list --installed $(ls depends/*amd64.deb |cut -d/ -f2|cut -d_ -f1)|grep amd64|cut -d/ -f1 |sed 's/$/&*amd64.deb /g')
 #write LOG file
 apt list --installed $(ls depends/*i386.deb |cut -d/ -f2|cut -d_ -f1)|grep i386 > original_version.log
 apt list --installed $(ls depends/*amd64.deb |cut -d/ -f2|cut -d_ -f1)|grep amd64 >> original_version.log
-#amd64 packages
-#amd64_insed=$(apt list --installed $(ls depends/*amd64.deb |cut -d/ -f2|cut -d_ -f1)|grep amd64|cut -d/ -f1)
-#amd64_ins=$(echo $amd64_insed |sed 's/ /*amd64.deb /g'|sed 's/$/&*amd64.deb/g')
-amd64_ins=$(apt list --installed $(ls depends/*amd64.deb |cut -d/ -f2|cut -d_ -f1)|grep amd64|cut -d/ -f1|sed 's/$/&*amd64.deb/g')
-
 #echo upgrade-able packages
 cat original_version.log
 echo "
@@ -46,7 +41,7 @@ if [ $test_opt -eq 0 ] ;then
     if [ $test_opt_v -eq 0 ] ;then
         dpkg -i $deb_ins
         apt -f install
-#check depends
+##check depends
 	depends="libva-x11-2 libva2 libva-drm2 libva-glx2 libva-wayland2 vainfo"
 	depends_insed=$(apt list --installed $depends|cut -d/ -f1|cut -d_ -f1)
 	for i in $depends
@@ -94,7 +89,7 @@ if [ $test_opt -eq 0 ] ;then
     else
         echo "Depends check OK"
     fi
-#install nvidia-vaapi-driver
+##install nvidia-vaapi-driver
     read -p "安装nvidia-vaapi-driver？[y/N]" opt_v2
     if [ $opt_v2 ] ;then
         echo $opt_v2
@@ -126,7 +121,3 @@ else
     echo "Please Enter \"Y\" for Yes or \"N\" for No, script stop"
     exit 0
 fi
-    
-#dpkg -i $deb_ins
-#dpkg -i /deb/*.deb
-#apt -f install
